b87c35187ea4302e9a60b52ec530d231242381c1
KajalBansal25 <kajal.bansal@daffodilsw.com>
2022-05-11 12:36:13 +0530

    header of post screen completed

--- src/components/newPost/AddNewPost.js
+++ src/components/newPost/AddNewPost.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import {Text, View, Image, TouchableOpacity, StyleSheet} from 'react-native';
 
- const AddNewPost=()=> {
-    return (
-      <View>
-        <Text style={{color:"white"}}> Add New Post</Text>
-      </View>
-    )
-  }
+const AddNewPost = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+    </View>
+  );
+};
 
+const Header = () => (
+  <View style={styles.headerContainer}>
+    <TouchableOpacity>
+      <Image
+        source={{uri: 'https://img.icons8.com/ios-glyphs/90/ffffff/back.png'}}
+        style={{height: 30, width: 30, tintColor: 'white'}}
+      />
+    </TouchableOpacity>
+    <Text style={styles.headerText}> New Post</Text>
+    <Text></Text>
+  </View>
+);
 
 export default AddNewPost;
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 10,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerText: {
+      color:"#fff",fontWeight:"600",fontSize:20,
+      marginLeft:25,
+  },
+});
